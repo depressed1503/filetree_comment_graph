d70da5d217447ce3f594061ba322a5ae57b4b20b
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-30 09:56:40 +0300

    Fixed something within setup.

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     name="filetree_comment_graph",
     version="0.1",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "filetree_comment_graph=filetree_comment_graph.__main__:main",
+        ],
+    },
 )

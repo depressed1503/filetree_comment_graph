70f78fc3f88e929935ce9e71b7aa9d644fd117c3
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-30 11:46:44 +0300

    Updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 # <comment>Сетап файл для выпуска .whl пакета</comment>
 from setuptools import setup, find_packages
 
+print(find_packages)
 setup(
     name="filetree_comment_graph",
     version="0.1",
-    packages=find_packages(include=["filetree_comment_graph"]),
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "filetree_comment_graph=filetree_comment_graph.__main__:main",
